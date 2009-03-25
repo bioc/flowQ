83b6ebbadc6133e45a78b4e8176392aebc53ea7f
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2009-03-25 00:42:31 +0000

    using shell instead of sytem to make this work on windows
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowQ@38167 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 ## check for a valid ImageMagick installation
 .onAttach <- function(...)
 {
-    vers <- system("convert -list configure", intern=TRUE, ignore.stderr=TRUE)
-    path <- gsub("Path: *", "", grep("Path:", vers, value=TRUE))
-    vers <- gsub("VERSION *", "", grep("^VERSION", vers, value=TRUE))
+    vers <- shell("convert -list configure", intern=TRUE, ignore.stderr=TRUE)
+    path <- gsub("Path: *", "", grep("Path:", vers, value=TRUE))[1]
+    vers <- gsub("VERSION *", "", grep("^VERSION", vers, value=TRUE))[1]
     if(!length(vers) || !length(path))
         stop("\n\nUnable to find installation of ImageMagick on this system.\n",
              "Please install the ImageMagick library (http://www.imagemagick.org)\n",
